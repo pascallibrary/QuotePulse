8497d77a5d2b9a531632e9cb1c27b1843999e0e0
pascal <pascalokereke18@gmail.com>
2025-06-23 08:27:03 +0100

    done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,4 +6,4 @@ const { registerUser, loginUser } = require('../controllers/authController');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
-module.exports = router;
+module.exports = router; 
