747ef48ac7744a7414791b820e5adebb73fd8677
pascal <pascalokereke18@gmail.com>
2025-06-30 15:18:12 +0100

    mongodb successfully works cause i connected the db.

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-// main entry of the application
+require('dotenv').config();
 
-const app = require('./src/app'); // Import the Express Application
-const connectDB = require('./src/utils/db'); // Import the database connection utility
-const config = require('./src/config')
+const app = require('./src/app');
+const connectDB = require('./src/utils/db');
+const config = require('./src/config');
 
 const PORT = config.PORT;
 
+// connect to the db here
 
-
-
-// start the server 
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`)
-    console.log(`Access the API at http://localhost:${PORT}`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server running on port ${PORT}');
+        console.log(`Access the API at http://localhost:${PORT}`);
+    });
 })
+    .catch((err) => {
+    console.error('Could not start server due to DB connection error:', err);
+});
