10b87633adf95f52cedea3ec059d4144d9889f5e
pascal <pascalokereke18@gmail.com>
2025-07-11 15:26:09 +0100

    rm the export

--- quotepulse/next.config.ts
+++ quotepulse/next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // optional — for static exports
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
